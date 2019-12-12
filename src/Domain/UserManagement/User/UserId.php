7d39c7a2a7d89124b297d5ea8d91fd7b9ad51bf1
Filipe Silva <silvam.filipe@gmail.com>
2019-12-12 00:48:14 -0100

    Adds base file structure and OAuth2 support

---
+++ src/Domain/UserManagement/User/UserId.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of S4Skeleton project
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Domain\UserManagement\User;
+
+use App\Domain\Common\RootAggregatorId;
+
+/**
+ * UserId
+ *
+ * @package App\Domain\UserManagement\User
+ */
+final class UserId extends RootAggregatorId
+{
+
+}

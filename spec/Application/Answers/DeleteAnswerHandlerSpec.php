5f35f328950591d380f6cd30fd1d5b9b69fd624e
Joshua Alves <joshua.s.alves@gmail.com>
2020-01-19 19:41:40 -0100

    added DeleteAnswerCommand/Handler

--- spec/Application/Answers/DeleteAnswerHandlerSpec.php
+++ spec/Application/Answers/DeleteAnswerHandlerSpec.php
@@ -2,16 +2,90 @@
 
 namespace spec\App\Application\Answers;
 
+use App\Application\Answers\DeleteAnswerCommand;
 use App\Application\Answers\DeleteAnswerHandler;
+use App\Domain\Answers\Answer;
+use App\Domain\Answers\Answer\AnswerId;
+use App\Domain\Answers\AnswersRepository;
+use App\Domain\Answers\Specification\AcceptedAnswer;
+use App\Domain\Answers\Specification\AnswerOwner;
+use App\Domain\Events\EventPublisher;
+use App\Domain\Exceptions\InvalidAnswerOwner;
+use App\Domain\Exceptions\InvalidAnswerState;
+use App\Domain\Questions\Question;
+use App\Domain\Questions\Question\QuestionId;
+use App\Domain\Questions\QuestionsRepository;
 use PhpSpec\ObjectBehavior;
 
 class DeleteAnswerHandlerSpec extends ObjectBehavior
 {
 
+    private $answerId;
+    private $questionId;
 
+    function let(
+        AnswersRepository $answers,
+        QuestionsRepository $questions,
+        AnswerOwner $answerOwner,
+        AcceptedAnswer $acceptedAnswer,
+        EventPublisher $eventPublisher,
+        Question $question,
+        Answer $answer
+    ) {
+        $this->questionId = new QuestionId();
+        $questions->withId($this->questionId)->willReturn($question);
+
+        $this->answerId = new AnswerId();
+        $answers->withId($this->answerId)->willReturn($answer);
+
+        $answerOwner->isSatisfiedBy($answer)->willReturn(true);
+        $acceptedAnswer->isSatisfiedBy($answer)->willReturn(true);
+
+        $this->beConstructedWith($answers, $answerOwner, $acceptedAnswer, $eventPublisher);
+    }
 
     function it_is_initializable()
     {
         $this->shouldHaveType(DeleteAnswerHandler::class);
+    }
+
+    function it_handles_delete_answer_command(
+        AnswersRepository $answers,
+        Answer $answer,
+        EventPublisher $eventPublisher
+    ) {
+        $command = new DeleteAnswerCommand(
+            $this->answerId
+        );
+
+        $this->handle($command)->shouldBe($answer);
+        $answers->remove($answer)->shouldHaveBeenCalled();
+        $eventPublisher->publishEventsFrom($answer)->shouldHaveBeenCalled();
+    }
+
+    function it_throws_an_exception_when_user_is_not_the_owner(
+        AnswerOwner $answerOwner,
+        Answer $answer
+    ) {
+        $command = new DeleteAnswerCommand(
+            $this->answerId
+        );
+
+        $answerOwner->isSatisfiedBy($answer)->willReturn(false);
+        $this->shouldThrow(InvalidAnswerOwner::class)
+            ->during('handle', [$command]);
+    }
+
+    function it_throws_an_exception_when_answer_is_already_accepted(
+        Answer $answer,
+        AcceptedAnswer $acceptedAnswer
+    ) {
+        $command = new DeleteAnswerCommand(
+            $this->answerId
+        );
+
+        $acceptedAnswer->isSatisfiedBy($answer)->willReturn(false);
+        $this->shouldThrow(InvalidAnswerState::class)
+            ->during('handle', [$command]);
     }
 }
--- src/Application/Answers/DeleteAnswerHandler.php
+++ src/Application/Answers/DeleteAnswerHandler.php
@@ -2,6 +2,67 @@
 
 namespace App\Application\Answers;
 
+use App\Domain\Answers\AnswersRepository;
+use App\Domain\Answers\Specification\AcceptedAnswer;
+use App\Domain\Answers\Specification\AnswerOwner;
+use App\Domain\Events\EventPublisher;
+use App\Domain\Exceptions\InvalidAnswerOwner;
+use App\Domain\Exceptions\InvalidAnswerState;
+
 class DeleteAnswerHandler
 {
+    /**
+     * @var AnswersRepository
+     */
+    private $answers;
+    /**
+     * @var AnswerOwner
+     */
+    private $answerOwner;
+    /**
+     * @var AcceptedAnswer
+     */
+    private $acceptedAnswer;
+    /**
+     * @var EventPublisher
+     */
+    private $eventPublisher;
+
+    /**
+     * DeleteAnswerHandler constructor.
+     * @param AnswersRepository $answers
+     * @param AnswerOwner $answerOwner
+     * @param AcceptedAnswer $acceptedAnswer
+     * @param EventPublisher $eventPublisher
+     */
+    public function __construct(
+        AnswersRepository $answers,
+        AnswerOwner $answerOwner,
+        AcceptedAnswer $acceptedAnswer,
+        EventPublisher $eventPublisher
+    ) {
+        $this->answers = $answers;
+        $this->answerOwner = $answerOwner;
+        $this->acceptedAnswer = $acceptedAnswer;
+        $this->eventPublisher = $eventPublisher;
+    }
+
+    public function handle(DeleteAnswerCommand $command)
+    {
+        $answer = $this->answers->withId($command->answerId());
+
+        if (!$this->answerOwner->isSatisfiedBy($answer)) {
+            throw new InvalidAnswerOwner(
+                "Only the answer's owner can delete this question."
+            );
+        }
+
+        if (!$this->acceptedAnswer->isSatisfiedBy($answer)) {
+            throw new InvalidAnswerState(
+                "Its only possible to delete an unaccepted answer. This answer is already accepted."
+            );
+        }
+
+        $this->eventPublisher->publishEventsFrom($this->answers->remove($answer));
+    }
 }
